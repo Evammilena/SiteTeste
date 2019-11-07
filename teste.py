1d7e800865e18252fd5c9357ee0a890bcccc5dc5
Ezequiel321carlos <ezequielcarlos119@gmail.com>
2019-11-07 15:32:22 -0300

    banco de dados criado

--- teste.py
+++ teste.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config[]'SQLALCHEMY_DATABASE_URL'] = 'sqlite://minhasmensagens.db'
+
+class Mensagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mensagem =  db.Column(db.Text, nullable=False) #nao pode  ser falsa
+
+    def __repre__(self):
+        return '<<<MSG: %r>>>' %self.mensagem
+
+
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route ("/nova-mensagem")
+@app.route ('/nova-mensagem')
 def novamensagem():
     return render_template("mensagens.html")
 
-if __name__ == "__main_" :
-
+if __name__=="__main_" :
     app.run(debug=True)
